25ca1e73cef465bb9070d8de614c3887655283f7
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2026-01-15 18:34:58 +0300

    refactor(server): :loud_sound: Update log messages in `inject_service` function

--- eventum/server/services/ui/injector.py
+++ eventum/server/services/ui/injector.py
@@ -26,11 +26,11 @@ def inject_service(
 
     """
     if not WWW_DIR.exists():
-        msg = 'www directory does not exist'
+        msg = 'Service static directory does not exist'
         raise ServiceBuildingError(msg, context={'path': str(WWW_DIR)})
 
     if not ASSETS_DIR.exists():
-        msg = 'www directory has not assets'
+        msg = 'Service static directory has not assets'
         raise ServiceBuildingError(msg, context={'path': str(ASSETS_DIR)})
 
     app.mount(
