2e0ecafc4f1169a100430ba571310cf94e4b1300
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2026-02-22 11:53:13 +0300

    fix: update GitHub organization link and documentation URL in LINKS in UI

--- eventum/ui/src/routing/links.ts
+++ eventum/ui/src/routing/links.ts
@@ -1,8 +1,8 @@
 export const LINKS = {
-  GITHUB_ORGANIZATION: 'https://github.com/eventum-project',
+  GITHUB_ORGANIZATION: 'https://github.com/eventum-generator',
   GITHUB_ISSUES: 'https://github.com/eventum-project/eventum-generator/issues',
   TG_COMMUNITY_GROUP: 'https://t.me/+5aw8dFk84W1jNTg6',
-  DOCUMENTATION: 'https://eventum-project.github.io/website/',
+  DOCUMENTATION: 'https://eventum.run',
   LICENSE:
     'https://github.com/eventum-project/eventum-generator/blob/master/LICENSE',
 };
