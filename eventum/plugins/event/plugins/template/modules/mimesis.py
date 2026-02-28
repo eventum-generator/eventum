a5a55668ed440bce71d65b7bfcbb8a8fe0a29329
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2026-02-28 16:20:55 +0300

    refactor: remove unused spec providers and related class from mimesis module

--- eventum/plugins/event/plugins/template/modules/mimesis.py
+++ eventum/plugins/event/plugins/template/modules/mimesis.py
@@ -2,17 +2,7 @@
 
 import mimesis.enums as _enums
 import mimesis.random as _random
-from mimesis import BaseDataProvider, Generic, Locale
-from mimesis.builtins import (
-    BrazilSpecProvider,
-    DenmarkSpecProvider,
-    ItalySpecProvider,
-    NetherlandsSpecProvider,
-    PolandSpecProvider,
-    RussiaSpecProvider,
-    UkraineSpecProvider,
-    USASpecProvider,
-)
+from mimesis import Generic, Locale
 
 
 class _Locale:
@@ -22,6 +12,7 @@ class _Locale:
     def __getitem__(self, locale: str) -> Generic:
         if locale in self._dict:
             return self._dict[locale]
+
         try:
             generator = Generic(Locale(locale))
         except ValueError:
@@ -32,35 +23,7 @@ class _Locale:
         return generator
 
 
-class _Spec:
-    def __init__(self) -> None:
-        self._dict: dict[str, BaseDataProvider] = {}
-
-    def __getitem__(self, spec_name: str) -> BaseDataProvider:
-        if spec_name in self._dict:
-            return self._dict[spec_name]
-
-        try:
-            spec: BaseDataProvider = {
-                'brazil': BrazilSpecProvider(),
-                'denmark': DenmarkSpecProvider(),
-                'italy': ItalySpecProvider(),
-                'netherlands': NetherlandsSpecProvider(),
-                'poland': PolandSpecProvider(),
-                'russia': RussiaSpecProvider(),
-                'ukraine': UkraineSpecProvider(),
-                'usa': USASpecProvider(),
-            }[spec_name]
-        except KeyError as e:
-            msg = f'Unknown spec `{e}`'
-            raise KeyError(msg) from None
-
-        self._dict[spec_name] = spec
-        return spec
-
-
 enums = _enums
 random = _random
 
 locale = _Locale()
-spec = _Spec()
